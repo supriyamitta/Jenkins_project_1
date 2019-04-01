679aa24dfa3406471057c33885231739510c667a
supriyamitta <supriyamitta@gmail.com>
2019-04-01 14:20:42 +0530

    added for loop again

--- com/learn/Hello.java
+++ com/learn/Hello.java
@@ -3,12 +3,12 @@ package com.learn;
 public class Hello {
 
 	public static void main(String[] args) {
-		//for ( int i=0;i<=10;i++){
+		for ( int i=0;i<=10;i++){
 			
-			   System.out.println("Hello Supriya.." );
+			   System.out.println("Hello Supriya.."+i );
 			   System.out.println("Adding another line test");
 			   System.out.println("Adding Second  line test");
-		//}
+		}
 	}
 
 }
